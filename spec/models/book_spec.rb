1ea1f90bc683513bd8fdfc1557bd5b8d302ad06e
Arlisson Jaques <arlissonjaques@gmail.com>
2021-01-10 18:11:58 -0300

    test(book_model): added validations tests

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:price) }
+    it { should validate_numericality_of(:price) }
+  end
+
 end
