d48da83106a0036c991fc318f0ab370e747e9a10
Arlisson Jaques <arlissonjaques@gmail.com>
2021-01-10 18:27:13 -0300

    refactor(client_model): additional validation

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,6 +2,7 @@ class Client < ApplicationRecord
   has_many :orders
   has_many :form_of_payments, through: :orders
 
-  validates :firstname, :lastname, presence: true
-  validates_format_of :email, :with => /\A[^@\s]+@[^@\s]+\z/
+  validates :firstname, :lastname, :email, presence: true
+  validates :email, uniqueness: true
+  validates_format_of :email, with: /\A[^@\s]+@[^@\s]+\z/
 end
