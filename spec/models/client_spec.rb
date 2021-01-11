6532aa3199b0ef8540c53e8f0e51575ecefaba8c
Arlisson Jaques <arlissonjaques@gmail.com>
2021-01-10 22:18:01 -0300

    test(client_model): added associations tests

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe Client, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email)}
   end
+
+  describe 'associations' do
+    it { should have_many(:form_of_payments).through(:orders) }
+  end
+
 end
