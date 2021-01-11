ff9cc48c8aaef87ed9d0ce66110c9202cb9f575f
Arlisson Jaques <arlissonjaques@gmail.com>
2021-01-10 22:08:46 -0300

    test(client_model): added validations tests

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Client, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+    it { should validate_presence_of(:firstname) }
+    it { should validate_presence_of(:lastname) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email)}
+  end
 end
