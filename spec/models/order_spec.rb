fba43b0c91eff948976a0c581d76119abef48193
Arlisson Jaques <arlissonjaques@gmail.com>
2021-01-11 20:09:16 -0300

    test(order_model): added associations tests

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'associations' do
+    it { should belong_to(:client) }
+    it { should belong_to(:book) }
+    it { should belong_to(:form_of_payment) }
+  end
+
 end
