0cf5daa95e82e92e014ce55ec3a9a5a027ad86e0
Arlisson Jaques <arlissonjaques@gmail.com>
2021-01-11 13:24:27 -0300

    refactor(form_of_payment_model): additional validation

--- app/models/form_of_payment.rb
+++ app/models/form_of_payment.rb
@@ -2,5 +2,5 @@ class FormOfPayment < ApplicationRecord
   has_many :orders
   has_many :clients, through: :orders
 
-  validates :type_payment, presence: true
+  validates :type_payment, presence: true, uniqueness: true
 end
