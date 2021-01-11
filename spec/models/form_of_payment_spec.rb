b674863a7310c646e9f4c7556c482ae44367e56d
Arlisson Jaques <arlissonjaques@gmail.com>
2021-01-11 13:38:27 -0300

    test(form_of_payment_model): added associations tests

--- spec/models/form_of_payment_spec.rb
+++ spec/models/form_of_payment_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe FormOfPayment, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:type_payment) }
   end
+
+  describe 'associations' do
+    it { should have_many(:clients).through(:orders) }
+  end
 end
