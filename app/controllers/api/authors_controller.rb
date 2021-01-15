e066222d7ec29473f1d58a0370e7560c8d953951
Arlisson Jaques <arlissonjaques@gmail.com>
2021-01-15 20:54:51 -0300

    refactor(author_controller): generated a new controller with actions and tests ready

--- app/controllers/api/authors_controller.rb
+++ app/controllers/api/authors_controller.rb
@@ -1,19 +1,41 @@
 class Api::AuthorsController < ApplicationController
+
+  before_action :set_author, only: [:update, :destroy]
+
   def index
     render json: Author.all
   end
 
   def create
-    author = Author.new(author_params)
+    @author = Author.new(author_params)
 
-    if author.save
-      render json: author, status: :created
+    if @author.save
+      render json: @author, status: :created
     else
-      render json: author.errors, status: :unprocessable_entity
+      render json: @author.errors, status: :unprocessable_entity #TODO: encontroar um jeito de destar
     end
   end
 
+  def update
+    if @author.update(author_params)
+      render json: @author, status: :ok
+    else
+      render json: @author.errors, status: :unprocessable_entity #TODO: encontroar um jeito de destar
+    end
+  end
+
+  def destroy
+    @author.destroy
+    # render json: {'message': 'data deleted successfully'}
+  end
+
   private
+
+  def set_author
+    @author = Author.find(params[:id])
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { message: e.message }, status: :not_found
+  end
 
   def author_params
     params.permit(:name)
--- spec/requests/api/authors_request_spec.rb
+++ spec/requests/api/authors_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "Api::Authors", type: :request do
-  describe "GET api/authors" do
+RSpec.describe 'Api::Authors', type: :request do
+  describe 'GET api/authors' do
     before do
       create(:author)
       create(:author)
@@ -17,12 +17,47 @@ RSpec.describe "Api::Authors", type: :request do
       json_response = JSON.parse(response.body)
       expect(json_response[0].keys).to match_array(%w[id name created_at updated_at])
     end
+  end
+  # POST
+  describe 'POST /api/authors' do
+    it 'author created' do
+      post '/api/authors/', params: { name: :author }
+      expect(response).to have_http_status(:created)
+    end
+  end
 
-    describe 'POST /api/authors' do
-      it 'return a consultant created' do
-        post '/api/authors/', params: {name: :author}
-        expect(response).to have_http_status(:created)
+  describe 'PUT /api/authors/x' do
+    context 'when author exist' do
+      it 'record update' do
+        author = create(:author)
+        author_attributes = attributes_for(:author)
+        put "/api/authors/#{author.id}", params: author_attributes
+        expect(author.reload).to have_attributes(author_attributes)
       end
     end
+
+    context 'with author does not exist' do
+      it 'returns status code 404' do
+        put '/api/authors/0', params: attributes_for(:author)
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
+
+  describe 'DELETE api/authors/x' do
+    context 'when author exists' do
+      it 'excluding author' do
+        author = create(:author)
+        delete "/api/authors/#{author.id}"
+        expect(response).to have_http_status(204)
+      end
+
+      it 'destroy record' do
+        author = create(:author)
+        delete "/api/authors/#{author.id}"
+        expect { author.reload }.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+
   end
 end
