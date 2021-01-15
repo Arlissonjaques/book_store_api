e9e1ef74f795f30251fbe23bf58aa4c072420889
Arlisson Jaques <arlissonjaques@gmail.com>
2021-01-15 19:36:54 -0300

    feat(authors_controller): post action added

--- app/controllers/api/authors_controller.rb
+++ app/controllers/api/authors_controller.rb
@@ -2,4 +2,21 @@ class Api::AuthorsController < ApplicationController
   def index
     render json: Author.all
   end
+
+  def create
+    author = Author.new(author_params)
+
+    if author.save
+      render json: author, status: :created
+    else
+      render json: author.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def author_params
+    params.permit(:name)
+  end
+
 end
