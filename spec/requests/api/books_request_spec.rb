109e513ea7edf23bacc77e9009bc245320bf8dc4
Arlisson Jaques <arlissonjaques@gmail.com>
2021-01-17 20:39:24 -0300

    tests(books_controller): add tests the update action

--- spec/requests/api/books_request_spec.rb
+++ spec/requests/api/books_request_spec.rb
@@ -37,4 +37,30 @@ RSpec.describe "Api::Books", type: :request do
     end
   end
 
+  describe 'PUT /api/books/x' do
+    context 'when book exist' do
+      it 'record update' do
+        book = create(:book)
+        book_attributes = attributes_for(:book)
+        put "/api/books/#{book.id}", params: book_attributes
+        expect(book.reload).to have_attributes(book_attributes)
+      end
+    end
+
+    context 'with invalid arguments' do
+      it 'book created' do
+        book = create(:book)
+        post '/api/books/', params: { name: book.name }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    context 'with book does not exist' do
+      it 'returns status code 404' do
+        put '/api/authors/0', params: attributes_for(:author)
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
+
 end
