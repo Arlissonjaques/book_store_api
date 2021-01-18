4ddb8abfaacd3a3fea89426a13fb037bd8ee97b3
Arlisson Jaques <arlissonjaques@gmail.com>
2021-01-17 23:40:21 -0300

    fix(clients_controller): tests that did not pass resolved

--- spec/requests/api/clients_request_spec.rb
+++ spec/requests/api/clients_request_spec.rb
@@ -38,15 +38,14 @@ RSpec.describe "Api::Clients", type: :request do
   end
 
   describe 'PUT /api/clients/x' do
-    # context 'when client exist' do
-      # it 'record update' do
-      #   client = create(:client)
-      #   client_attributes = attributes_for(:client)
-      #   put "/api/clients/#{client.id}", params: client_attributes
-      #   expect(client.reload).to have_attributes(client_attributes.slice(:firstname, :lastname, :email))
-      # end
-      #TODO: resolver o problema desse teste
-    # end
+    context 'when client exist' do
+      it 'record update' do
+        client = create(:client)
+        client_attributes = attributes_for(:client)
+        put "/api/clients/#{client.id}", params: client_attributes
+        expect(client.reload).to have_attributes(client_attributes.slice(:firstname, :lastname, :email))
+      end
+    end
 
     context 'with invalid arguments' do
       it 'client created' do
