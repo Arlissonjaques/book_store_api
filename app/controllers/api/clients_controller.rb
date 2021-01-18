62c3aa028da1c94c359cc27bf48c5744c8e87dac
Arlisson Jaques <arlissonjaques@gmail.com>
2021-01-17 22:47:12 -0300

    feat(clients_controller): create action added

--- app/controllers/api/clients_controller.rb
+++ app/controllers/api/clients_controller.rb
@@ -4,4 +4,20 @@ class Api::ClientsController < ApplicationController
     render json: Client.all
   end
 
+  def create
+    @client = Client.new(client_params)
+
+    if @client.save
+      render json: @client, status: :created
+    else
+      render json: @client.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def client_params
+    params.permit(:firstname, :lastname, :email)
+  end
+
 end
