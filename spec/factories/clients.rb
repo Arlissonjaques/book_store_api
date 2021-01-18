ee50b132770866ec411f914097ab1cabd925c1d7
Arlisson Jaques <arlissonjaques@gmail.com>
2021-01-17 23:07:06 -0300

    refactor(factory): factory parameter updated

--- spec/factories/clients.rb
+++ spec/factories/clients.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :client do
     firstname { Faker::Name.first_name }
     lastname { Faker::Name.last_name }
-    email { Faker::Internet.email }
+    email { "email@teste.com"}
   end
 end
