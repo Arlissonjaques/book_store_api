08820c9890a8f8a042493eebaa85d0327bf303e6
Arlisson Jaques <arlissonjaques@gmail.com>
2021-01-17 23:03:30 -0300

    test(clients_controller): test the update action

--- spec/requests/api/clients_request_spec.rb
+++ spec/requests/api/clients_request_spec.rb
@@ -36,4 +36,31 @@ RSpec.describe "Api::Clients", type: :request do
       end
     end
   end
+
+  describe 'PUT /api/clients/x' do
+    # context 'when client exist' do
+      # it 'record update' do
+      #   client = create(:client)
+      #   client_attributes = attributes_for(:client)
+      #   put "/api/clients/#{client.id}", params: client_attributes
+      #   expect(client.reload).to have_attributes(client_attributes.slice(:firstname, :lastname, :email))
+      # end
+      #TODO: resolver o problema desse teste
+    # end
+
+    context 'with invalid arguments' do
+      it 'client created' do
+        client = create(:client)
+        post '/api/clients/', params: { name: client.firstname }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    context 'with client does not exist' do
+      it 'returns status code 404' do
+        put '/api/clients/0', params: attributes_for(:client)
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
 end
