c2602d8b64d48c417db9aa2e5c6c56dbd22f99a8
Arlisson Jaques <arlissonjaques@gmail.com>
2021-01-18 22:41:42 -0300

    test(form_of_payments_controller): test the update action

--- spec/requests/api/form_of_payments_request_spec.rb
+++ spec/requests/api/form_of_payments_request_spec.rb
@@ -35,6 +35,31 @@ RSpec.describe "Api::FormOfPayments", type: :request do
     end
   end
 
+  describe 'PUT /api/form_of_payments/x' do
+    context 'when form_of_payment exist' do
+      it 'record update' do
+        form_of_payment = create(:form_of_payment)
+        form_of_payment_attributes = attributes_for(:form_of_payment)
+        put "/api/form_of_payments/#{form_of_payment.id}", params: form_of_payment_attributes
+        expect(form_of_payment.reload).to have_attributes(form_of_payment_attributes.slice(:firstname, :lastname, :email))
+      end
+    end
+
+    context 'with invalid arguments' do
+      it 'form_of_payment created' do
+        post '/api/form_of_payments/', params: { type_payment: " " }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    context 'with form_of_payment does not exist' do
+      it 'returns status code 404' do
+        put '/api/form_of_payments/0', params: attributes_for(:form_of_payment)
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
+
   describe 'DELETE api/form_of_payments/x' do
     context 'when form_of_payment exists' do
       it 'excluding form_of_payment' do
