094fd1c040c872b7bc5457bf5375de03b4e59735
Arlisson Jaques <arlissonjaques@gmail.com>
2021-01-18 21:57:17 -0300

    feat(form_of_payments_controller): create action added

--- app/controllers/api/form_of_payments_controller.rb
+++ app/controllers/api/form_of_payments_controller.rb
@@ -3,4 +3,20 @@ class Api::FormOfPaymentsController < ApplicationController
   def index
     render json: FormOfPayment.all
   end
+
+  def create
+    @form_of_payment = FormOfPayment.new(form_of_payment_params)
+
+    if @form_of_payment.save
+      render json: @form_of_payment, status: :created
+    else
+      render json: @form_of_payment.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def form_of_payment_params
+    params.permit(:type_payment)
+  end
 end
