e4b837b06bbce1c185cdf83c4d3799659139f63f
Arlisson Jaques <arlissonjaques@gmail.com>
2021-01-25 21:43:21 -0300

    feat(orders_controller): post action added

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -4,4 +4,20 @@ class Api::OrdersController < ApplicationController
     render json: Order.all
   end
 
+  def create
+    @order = Order.new(order_params)
+
+    if @order.save
+      render json: @order, status: :created
+    else
+      render json: @order.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.permit(:client_id, :book_id, :form_of_payment_id)
+  end
+
 end
