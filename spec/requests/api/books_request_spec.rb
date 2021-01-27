6574126426b17dc0e4e413baeff7dbfc1540cf01
Arlisson Jaques <arlissonjaques@gmail.com>
2021-01-27 20:40:48 -0300

    refactor(book_request_spec): improving the tests

--- spec/requests/api/books_request_spec.rb
+++ spec/requests/api/books_request_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe "Api::Books", type: :request do
   describe 'GET  api/books' do
     before do
-      create(:book)
-      create(:book)
+      create_list(:book, 2)
       get '/api/books'
     end
 
@@ -15,7 +14,7 @@ RSpec.describe "Api::Books", type: :request do
 
     it 'check JSON attributes' do
       json_response = JSON.parse(response.body)
-      expect(json_response[0].keys).to match_array(%w[id name price author_id created_at updated_at])
+      expect(json_response[0].keys).to include('id', 'name', 'author_id')
     end
   end
 
@@ -23,15 +22,14 @@ RSpec.describe "Api::Books", type: :request do
     context 'with valid arguments' do
       it 'book created' do
         book = create(:book)
-        post '/api/books/', params: { name: book.name, price: Faker::Number.within(range: 10..70), author_id: book.author_id }
+        post '/api/books/', params: { name: book.name, price: book.price, author_id: book.author_id }
         expect(response).to have_http_status(:created)
       end
     end
 
     context 'with invalid arguments' do
       it 'book created' do
-        author = create(:author)
-        post '/api/books/', params: { name: author.name }
+        post '/api/books/', params: { name: '' }
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
@@ -49,25 +47,17 @@ RSpec.describe "Api::Books", type: :request do
 
     context 'with invalid arguments' do
       it 'book created' do
-        book = create(:book)
-        post '/api/books/', params: { name: book.name }
-        expect(response).to have_http_status(:unprocessable_entity)
-      end
-    end
-
-    context 'with book does not exist' do
-      it 'returns status code 404' do
-        put '/api/authors/0', params: attributes_for(:author)
-        expect(response).to have_http_status(404)
+        put "/api/books/#{create(:book).id}", params: { name: '' }
+        expect(response).to have_http_status(422)
       end
     end
   end
 
   describe 'DELETE api/books/x' do
     context 'when book exists' do
+
       it 'excluding book' do
-        book = create(:book)
-        delete "/api/books/#{book.id}"
+        delete "/api/books/#{create(:book).id}"
         expect(response).to have_http_status(204)
       end
 
@@ -79,5 +69,4 @@ RSpec.describe "Api::Books", type: :request do
     end
 
   end
-
 end
